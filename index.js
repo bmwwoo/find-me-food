1c77d2ceede958e592f53b0fe2ed462fdd329624
Brandon Woo <bmw.woo@gmail.com>
2015-07-19 23:59:00 -0700

    Add initial file structure and create components

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ React = require("react");
 
 App = require("./components/layout/App");
 
+require("./assets/vendor/bootstrap/stylesheets/_bootstrap.scss");
+
 React.render(React.createElement(App), document.body);
